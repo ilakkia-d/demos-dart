995078923918bc2d723973729596c5203e6f44ed
ilakkia-d <ilakkia@asyncsolution.co>
2023-12-20 16:17:15 +0400

    Initial commit- demos and examples commit

---
+++ bin/demodart2.dart
@@ -0,0 +1,14 @@
+import 'package:demodart2/demodart2.dart' as demodart2;
+
+void main(){
+  print("hello world");
+  printname();
+}
+void printname()
+{
+  print('stackoverflow');
+}
+void printinfo(string name,string gender)
+{
+
+}
