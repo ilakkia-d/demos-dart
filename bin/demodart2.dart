f45124df52c484b018acd51df451789ff4c4386b
ilakkia-d <ilakkia@asyncsolution.co>
2023-12-22 21:31:14 +0400

    demos-for functions with parameters,anonymous, diff functions

--- bin/demodart2.dart
+++ bin/demodart2.dart
@@ -1,14 +1,82 @@
-import 'package:demodart2/demodart2.dart' as demodart2;
+
 
 void main(){
   print("hello world");
   printname();
+  printinfo("jose","male");
+  Register r=Register();
+  r.getData("samuel", "email");
+  r.showData();
+
+  String name =primeMinisterName();
+  print("the name from the function is $name ");
+  String personaldetail="from india";
+  if(personaldetail!=voterdetail())
+    {
+      print("you are not in india");
+
+    }
+  else
+    {
+      print("you are in india");
+    }
 }
 void printname()
 {
   print('stackoverflow');
 }
-void printinfo(string name,string gender)
+void printinfo(String name,String gender)
 {
+  print("Hello $name :your gender is $gender");
+}
+String primeMinisterName()
+{
+  return "pauljoseph";
+}
+//function with  No parameter and return type
+
+String voterdetail()
+{
+  return 'fromMuscat';
 
 }
+
+class Register
+{
+  late String  userName;
+  late String email;
+   late String password;
+    late String rePassword;
+    void getData(String n, String a )
+    {
+      userName = n;
+    }
+    void showData() {
+      print('userName=$userName');
+      print('email=$email');
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
