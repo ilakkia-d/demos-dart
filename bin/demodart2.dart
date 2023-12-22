fa830bd8050e312dc720f23ce02f474c893ad411
ilakkia-d <ilakkia@asyncsolution.co>
2023-12-22 21:59:23 +0400

    demos-for functions with parameters,anonymous, diff functions

--- bin/demodart2.dart
+++ bin/demodart2.dart
@@ -6,7 +6,12 @@ void main(){
   printinfo("jose","male");
   Register r=Register();
   r.getData("samuel", "email");
-  r.showData();
+  r.showdata();
+  Employee e = Employee('Siddh', 24000, 'Team Leader');
+  e.showData();
+
+  Employee e2 = Employee.admin('Mark', 50000);
+  e2.showData();
 
   String name =primeMinisterName();
   print("the name from the function is $name ");
@@ -51,32 +56,62 @@ class Register
     {
       userName = n;
     }
-    void showData() {
+    void showdata() {
       print('userName=$userName');
       print('email=$email');
     }
 
 
     }
+class Employee{
 
+  // fields
+  late String name;
+  late int salary;
+  late String role;
 
+  void showData(){
+    print('$name is $role & earns $salary');
+  }
 
+  // Default constructor
+//   Employee(){
+//     name = 'Sam';
+//     salary = 25000;
+//     role = 'Manager';
+//   }
 
+  // Parameterised constructor
+  Employee(String n, int s, String r){
+    name = n;
+    salary = s;
+    role = r;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
+  // Named constructor
+  Employee.admin(String n, int s){
+    name = n;
+    salary = s;
+    role = 'Admin';
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
